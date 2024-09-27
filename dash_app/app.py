7b889724249470e2d38cdaec2d078ec213a252aa
thefilesareinthecomputer <antonwintermn@gmail.com>
2024-09-27 12:27:22 -0500

    updated dynamic port in app script

--- dash_app/app.py
+++ dash_app/app.py
@@ -327,4 +327,7 @@ def update_sales_by_location(selected_region, selected_location, start_date, end
 
 # Run the app
 if __name__ == '__main__':
-    app.run_server(debug=True, host="0.0.0.0", port=8050)
+    # app.run_server(host='0.0.0.0', port=8050, debug=True)
+    # app.run_server(debug=True, host="0.0.0.0", port=8050)
+    port = int(os.environ.get("PORT", 8050))
+    app.run_server(host='0.0.0.0', port=port)
